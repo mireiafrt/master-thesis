2c955515bd2645704f1f5fa9ec2f86e4f86b8506
mireiafrt <mireiafl105@gmail.com>
2025-03-12 19:44:30 +0100

    method to create new destination image folder in main

--- src/data_filtering/data_filtering.py
+++ src/data_filtering/data_filtering.py
@@ -1,17 +1,28 @@
 import yaml
 import pandas as pd
 import numpy as np
+import os
+import shutil
+from tqdm import tqdm
+
 
 def load_config(path="config/data_filtering/data_filtering.yaml"):
     with open(path, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
-def get_valid_patient_ids(config_data_paths, config_subgroup_attributes, config_col_binary_target):
+
+def convert_age_numeric(df, age_col):
+    # convert to numeric and keep nulls as nan
+    df[age_col] = pd.to_numeric(df[age_col].str.rstrip('Y'), errors='coerce')
+
+
+def get_valid_patients(config_data_paths, config_subgroup_attributes, config_col_binary_target):
     """
     This function filters the patient metadata to keep patients with non null age and gender, as well as a diagnosis.
     Also, we only consider instances with CT scans modality.
-    It returns an array of patient ids fullfilling the above criteria.
+    It returns a dictionary with 'Patient ID' as key and 2 value attributes for 'Series Number' and 'Image Count'.
+    This dictionary will help filter the folder of images per patient.
     """
     # read patient metadata
     patient_df = pd.read_excel(config_data_paths['patient_metadata'])
@@ -40,27 +51,89 @@ def get_valid_patient_ids(config_data_paths, config_subgroup_attributes, config_
     # save the new filtered metadata file for later use
     df.to_csv(config_data_paths['new_metadata'], index=False)
 
-    # return patient ids in filtered metadata
-    return df['Patient ID'].unique()
+    # Filter the DataFrame to only include necessary columns to filter image folder
+    sub_df = df[['Patient ID', 'Series Number', 'Image Count']]
+    # Convert to a dictionary with 'Patient ID' as the key and the rest as a nested dictionary
+    filtering_criteria_dict = sub_df.set_index('Patient ID').apply(lambda row: row.to_dict(), axis=1).to_dict()
+    return filtering_criteria_dict
 
 
-def convert_age_numeric(df, age_col):
-    # convert to numeric and keep nulls as nan
-    df[age_col] = pd.to_numeric(df[age_col].str.rstrip('Y'), errors='coerce')
+def process_patients(patients_info, config_data_paths):
+    """
+    This function takes the original downloaded images dataset (with all patients, and image modalties), and filters it.
+    It only keeps patients obtained from the filtering of the metadata on CT modality, non null sex, age, and diagnosis.
+    It creates a new directory.
+
+    To ensure the right modality of images is kept for each valid patient, we compare the sub-folder names to the "Series Number",
+    and make sure the number of images in the subdirectory matches the "Image Count" data.
+
+    Additionally, each XML of the patients is also kept.
+    """
+
+    src_dir = config_data_paths['patient_images']
+    dst_dir = config_data_paths['new_images']
+
+    # Create the destination directory if it does not exist
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+    patient_folders = os.listdir(src_dir)
+    num_processed = 0  # Counter for tracking number of new valid patient folders
+
+    # Process each patient directory with a progress bar
+    for patient_folder in tqdm(patient_folders, desc='Processing patients', unit='folder'):
+        patient_id = patient_folder
+        # If the patient is in the valid patients dictionary
+        if patient_id in patients_info:
+            src_patient_path = os.path.join(src_dir, patient_folder)
+            dst_patient_path = os.path.join(dst_dir, patient_folder)
+
+            # Ensure destination patient folder exists
+            if not os.path.exists(dst_patient_path):
+                os.makedirs(dst_patient_path)
+                num_processed += 1  # Increment the count when a new folder is created
+
+            # Process each subfolder in the patient directory
+            for subfolder in os.listdir(src_patient_path):
+                subfolder_path = os.path.join(src_patient_path, subfolder)
+                if os.path.isdir(subfolder_path):
+                    # Check subsubfolders
+                    for subsubfolder in os.listdir(subfolder_path):
+                        subsubfolder_path = os.path.join(subfolder_path, subsubfolder)
+                        if os.path.isdir(subsubfolder_path):
+                            # Extract series number and check against dictionary of valid patients
+                            series_number = int(subsubfolder.split('-')[0])
+                            # Find XML and DICOM files
+                            xml_file = [f for f in os.listdir(subsubfolder_path) if f.endswith('.xml')]
+                            dicom_files = [f for f in os.listdir(subsubfolder_path) if f.endswith('.dcm')]
+                            
+                            # Check if this subsubfolder matches the patient info
+                            patient_series_number = int(patients_info[patient_id]['Series Number'])
+                            patient_image_count = int(patients_info[patient_id]['Image Count'])
+                            if series_number == patient_series_number and len(dicom_files) == patient_image_count:
+                                # Copy all DICOM files and the XML file to the new destination
+                                for file in dicom_files + xml_file:
+                                    shutil.copy(os.path.join(subsubfolder_path, file), dst_patient_path)
+
+    # Print summary information once the processing is complete
+    print(f"Data transfer complete. {num_processed} patient folders have been processed.")
 
 
 def main():
     # Load the configuration
     config = load_config()
 
-    # Access data paths and subgroup attributes from the configuration
+    # Access configuration parameters
     config_data_paths = config['data_paths']
     config_subgroup_attributes = config['subgroup_attributes']
     config_col_binary_target = config['col_binary_target']
 
     # get the patient ids we will keep
-    filtered_patient_ids = get_valid_patient_ids(config_data_paths, config_subgroup_attributes, config_col_binary_target)
-    print(len(filtered_patient_ids))
+    filtering_criteria_dict = get_valid_patients(config_data_paths, config_subgroup_attributes, config_col_binary_target)
+    print(len(filtering_criteria_dict))
+
+    # filter the images with the appropiate patients and image modalities, and save it to new directory
+    process_patients(filtering_criteria_dict, config_data_paths)
 
 if __name__ == "__main__":
     main()
