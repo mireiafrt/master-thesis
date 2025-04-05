c8fbc52b834c6e2a3701e0b55cbc42ee6f1f20a0
mireiafrt <mireiafl105@gmail.com>
2025-04-05 20:54:59 +0200

    fixed results path name

--- experiments/age_group_experiment.py
+++ experiments/age_group_experiment.py
@@ -14,7 +14,7 @@ import csv
 import random
 
 # ============== SETUP CONFIG ==============
-paths = {"metadata":"data/metadata.csv", "nifti_files":"data/preprocessed_nifti", "log_csv":"logs/gender_experiment_results.csv"}
+paths = {"metadata":"data/metadata.csv", "nifti_files":"data/preprocessed_nifti", "log_csv":"logs/age_experiment_results.csv"}
 columns = {"patient_id": "Patient ID", "diagnosis": "binary_diagnosis_patient", "gender": "Patient Sex", "age_group":"age_group"}
 training = {"batch_size": 4, "num_epochs": 20, "learning_rate": 0.0001, "resize": [128, 128, 128], "rotation_prob": 0.5,
             "N_train":15, "N_val":5, "N_test":20, "age_group": "40-60", "n_experiments": 10}
