afd4c51fc1b2b74eb76d55b8b4e965fb94586004
mireiafrt <mireiafl105@gmail.com>
2025-05-26 08:44:02 +0000

    deleted some unused imports

--- src/generator/autoencoder_training.py
+++ src/generator/autoencoder_training.py
@@ -1,26 +1,21 @@
 import os
 import shutil
-import tempfile
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 import torch
 import torch.nn.functional as F
-from monai import transforms
-from monai.apps import MedNISTDataset
-from monai.config import print_config
-from monai.data import DataLoader, Dataset
-from monai.utils import first, set_determinism
 from torch.cuda.amp import GradScaler, autocast
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
-from generative.inferers import LatentDiffusionInferer
+from monai import transforms
+from monai.data import DataLoader, Dataset
+from monai.utils import set_determinism
 from generative.losses.adversarial_loss import PatchAdversarialLoss
 from generative.losses.perceptual import PerceptualLoss
-from generative.networks.nets import AutoencoderKL, DiffusionModelUNet, PatchDiscriminator
-from generative.networks.schedulers import DDPMScheduler
+from generative.networks.nets import AutoencoderKL, PatchDiscriminator
 
 
 # set global seed
