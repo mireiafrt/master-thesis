fa9daa7d52fd701a68c7e301b845a40eed6daf13
mireiafrt <mireiafl105@gmail.com>
2025-06-19 12:44:10 +0000

    added some comments for utility experiments

---
+++ src/evaluation/utility/utility_training.py
@@ -0,0 +1,4 @@
+# TRTR --> Train one model with real test set and evaluate on hold out real (initial train+val)
+# TSTR ---> Train 5 models, each with 1 of thr 5 synthetic test sets, and evaluate on the hold out real
+
+# Compute the difference in metrics between each TRTR and TSTR pairs
